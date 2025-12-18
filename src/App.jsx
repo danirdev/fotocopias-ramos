8b7292ea0c17ad8b881352d70ac5d76143e3b6f5
danirdev <danielramos550455@gmail.com>
2025-12-17 23:33:14 -0300

    feat: Initialize React project with Vite and integrate Tailwind CSS.

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+const App = () =>
+{
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Fotocopias Ramos</h1>
+    </div>
+  )
+}
+
+export default App
